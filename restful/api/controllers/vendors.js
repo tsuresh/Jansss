7b6e13afaeab1f9fbf5962067f8768c97f8365f1
Sajitha Suranjana De SIlva <ssuranjana7@gmail.com>
2020-03-18 17:22:09 +0530

    vendor receiving modules added

--- restful/api/controllers/vendors.js
+++ restful/api/controllers/vendors.js
@@ -1,9 +1,25 @@
-exports.get_all_vendors = (req, res, next) => {
+const User = require("../models/user");
+const Vendor = require("../models/vendor");
+const mongoose = require("mongoose");
+const express = require("express");
 
+
+exports.get_all_vendors = (req, res, next) => {
+    Vendor.find() //fetches all the vendors
+        .then(result => {
+            res.send(result);
+        }).catch(err => {
+            res.status(400).send(err);
+        })
 };
 
 exports.get_matching_vendors = (req, res, next) => {
-
+    Vendor.findById(req.params.id) //filters the vendors by Id
+        .then(result => {
+            res.send(result);
+        }).catch(err => {
+            res.status(400).send("Something went wrong." + err);
+        })
 };
 
 exports.pass_camp_details = (req, res, next) => {
