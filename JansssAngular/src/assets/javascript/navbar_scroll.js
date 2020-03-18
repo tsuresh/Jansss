406c8398f772d6fc495976e5af62a9e31d04f588
Shyami Abeysekera <w1742315@my.westminster.ac.uk>
2020-03-19 01:01:26 +0530

    Angular Project - Navigation bar scroll effect update

--- JansssAngular/src/assets/javascript/navbar_scroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-}
