8eb389823c851444bad1d6b0f21d3772f8404985
Sajitha Suranjana De SIlva <ssuranjana7@gmail.com>
2020-03-18 17:22:54 +0530

    vendor routings defined

--- restful/api/routes/vendors.js
+++ restful/api/routes/vendors.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const VendorsController = require('../controllers/vendors');
 
-router.post('/all', VendorsController.get_all_vendors);
+router.get('/all', VendorsController.get_all_vendors);
 
-router.post('/matching', VendorsController.get_matching_vendors);
+router.get('/match/:id', VendorsController.get_matching_vendors);
 
 module.exports = router;
