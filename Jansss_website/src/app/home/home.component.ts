da68569f6177c9b84e2425083fee538931587d0b
Anjalika De Silva <anjalikadesilva77@gmail.com>
2020-03-20 13:12:05 +0530

    landing page added

---
+++ Jansss_website/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
+
+window.onscroll = function() { slideInContent() };
+
+function slideInContent() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    document.getElementById("intro").className = "animated fadeInUp";
+    document.getElementById("logo").className = "animated slideInRight logo-left";
+    document.getElementById("arrow").className = "animated fadeIn arrow-down";
+    document.getElementById("intro").style.visibility = "visible";
+    document.getElementById("arrow").style.visibility = "visible";
+  } else {
+    document.getElementById("intro").className = "";
+    document.getElementById("logo").className = "animated fadeInUp";
+    document.getElementById("intro").style.visibility = "hidden";
+    document.getElementById("arrow").style.visibility = "hidden";
+  }
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: [
+      './home.component.scss',
+      '../../../node_modules/animate.css/animate.min.css'
+  ]
+})
+
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    slideInContent();
+  }
+
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#E06F63';
+  }
+}
