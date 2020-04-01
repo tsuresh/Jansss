b716d489bf20614517d999f40b8c3a20ec2ed30c
Suresh Peiris <tsmpmail@gmail.com>
2020-04-01 12:13:03 +0530

    Fixed some bugs with UI

--- JansssAngular/src/app/app.module.ts
+++ JansssAngular/src/app/app.module.ts
@@ -8,9 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { VendorSignUpComponent } from './vendor-sign-up/vendor-sign-up.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { EnterDetailsFormComponent } from './enter-details-form/enter-details-form.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
@@ -20,7 +18,7 @@ import { ClientSignUpComponent } from './client-sign-up/client-sign-up.component
 import { ClientLogInComponent } from './client-log-in/client-log-in.component';
 import { SelectCampaignComponent } from './select-campaign/select-campaign.component';
 import { SelectPaymentComponent } from './select-payment/select-payment.component';
->>>>>>> f4e0f98f3e06fb1b0a73512f0efe2b28766f7f7e
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -41,14 +39,11 @@ import { SelectPaymentComponent } from './select-payment/select-payment.componen
     SelectPaymentComponent,
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    BrowserAnimationsModule
-=======
     ReactiveFormsModule,
     FormsModule
->>>>>>> f4e0f98f3e06fb1b0a73512f0efe2b28766f7f7e
   ],
   providers: [],
   bootstrap: [AppComponent]
