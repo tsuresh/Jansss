7cdfdf55256a1cfb344fde7ffe53d2d39a7bf714
Shyami Abeysekera <shyami.r.abeysekera@gmail.com>
2020-04-11 22:52:59 +0530

    Angular Project - How it works media query

--- JansssAngular/src/app/home/home.component.ts
+++ JansssAngular/src/app/home/home.component.ts
@@ -32,7 +32,7 @@ function slideInContent() {
 export class HomeComponent implements OnInit {
   constructor(private router: Router) { }
   ngOnInit() {
-      // tslint:disable-next-line:only-arrow-functions
+    // tslint:disable-next-line:only-arrow-functions
     if (this.router.url === '/') {
       // alert('hi');
       document.getElementById('navbar').style.visibility = 'hidden';
@@ -49,255 +49,264 @@ export class HomeComponent implements OnInit {
     $('document').ready(function() {
       // tslint:disable-next-line:prefer-const
       let fixmeTop = $('.fixme').offset().top;
+      // tslint:disable-next-line:prefer-const
+      let windowSize = $(window).width();
       // tslint:disable-next-line:only-arrow-functions
-      $(window).scroll(function() {
-        // tslint:disable-next-line:prefer-const
-        let currentScroll = $(window).scrollTop();
-        if ((currentScroll >= fixmeTop - 20) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'visible',
-          });
-        } else if ((currentScroll >= fixmeTop - 40) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'visible'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '1',
-          });
-          $('.heading').css({
-            visibility: 'hidden',
-          });
-        } else if ((currentScroll >= fixmeTop - 60) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'visible'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '1',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'hidden',
-          });
-        } else if ((currentScroll >= fixmeTop - 80) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'visible'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '1',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'hidden',
-          });
-        } else if ((currentScroll >= fixmeTop - 100) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'visible'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '1',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'hidden',
-          });
-        } else if ((currentScroll >= fixmeTop - 120) && (currentScroll < fixmeTop)) {
-          $('.fixme').css({
-            position: 'fixed',
-            top: '15%',
-            margin: 'auto auto',
-            width: '88%'
-          });
-          // $('#fix').addClass('fix');
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'visible',
-          });
-        } else {
-          $('.fixme').css({
-            position: 'static',
-            margin: 'auto auto',
-            width: '100%'
-          });
-          $('#description1').css({
-            visibility: 'hidden'
-          });
-          $('#description2').css({
-            visibility: 'hidden'
-          });
-          $('#description3').css({
-            visibility: 'hidden'
-          });
-          $('#description4').css({
-            visibility: 'hidden'
-          });
-          $('.opaque1').css({
-            opacity: '0.5',
-          });
-          $('.opaque2').css({
-            opacity: '0.5',
-          });
-          $('.opaque3').css({
-            opacity: '0.5',
-          });
-          $('.opaque4').css({
-            opacity: '0.5',
-          });
-          $('.heading').css({
-            visibility: 'visible',
-          });
-        }
+      $(window).resize(function() {
+        fixmeTop = $('.fixme').offset().top;
+        windowSize = $(window).width();
       });
+      // if  (windowSize >= 1100) {
+        // tslint:disable-next-line:only-arrow-functions
+      $(window).scroll(function() {
+          // tslint:disable-next-line:prefer-const
+          let currentScroll = $(window).scrollTop();
+          if ((currentScroll >= fixmeTop - 20) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'visible',
+            });
+          } else if ((currentScroll >= fixmeTop - 40) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'visible'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '1',
+            });
+            $('.heading').css({
+              visibility: 'hidden',
+            });
+          } else if ((currentScroll >= fixmeTop - 60) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'visible'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '1',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'hidden',
+            });
+          } else if ((currentScroll >= fixmeTop - 80) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'visible'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '1',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'hidden',
+            });
+          } else if ((currentScroll >= fixmeTop - 100) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'visible'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '1',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'hidden',
+            });
+          } else if ((currentScroll >= fixmeTop - 120) && (currentScroll < fixmeTop)) {
+            $('.fixme').css({
+              position: 'fixed',
+              top: '15%',
+              margin: 'auto auto',
+              width: '88%'
+            });
+            $('#fix').addClass('container');
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'visible',
+            });
+          } else {
+            $('.fixme').css({
+              position: 'static',
+              margin: 'auto auto',
+              width: '100%'
+            });
+            $('#description1').css({
+              visibility: 'hidden'
+            });
+            $('#description2').css({
+              visibility: 'hidden'
+            });
+            $('#description3').css({
+              visibility: 'hidden'
+            });
+            $('#description4').css({
+              visibility: 'hidden'
+            });
+            $('.opaque1').css({
+              opacity: '0.5',
+            });
+            $('.opaque2').css({
+              opacity: '0.5',
+            });
+            $('.opaque3').css({
+              opacity: '0.5',
+            });
+            $('.opaque4').css({
+              opacity: '0.5',
+            });
+            $('.heading').css({
+              visibility: 'visible',
+            });
+          }
+        });
+      // }
     });
   }
 }
