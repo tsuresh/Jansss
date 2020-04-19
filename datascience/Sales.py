1de5d4a0eba3609a978e69f4a1a4f281b64e98fd
Jihan Jeeth <jeeth.2018453@iit.ac.lk>
2020-04-19 17:20:07 +0530

    Added the model to predict the sales

---
+++ datascience/Sales.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets, linear_model, metrics 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split 
+  
+
+dataset = pd.read_csv("Category.csv")
+dataset = pd.DataFrame(dataset)
+
+data = []
+for i in range(len(dataset)) : 
+    tv        = (((dataset.loc[i, "TV"])/dataset['TV'].max())*100)
+    radio     = (((dataset.loc[i, "radio"])/dataset['radio'].max())*100)
+    newspaper = (((dataset.loc[i, "newspaper"])/dataset['newspaper'].max())*100)
+    sales     = (((dataset.loc[i, "sales"])/dataset['sales'].max())*100)
+    data.append([tv,radio,newspaper,sales])
+
+data = pd.DataFrame(data)
+data.columns = ['TV', 'RADIO', 'NEWSPAPER', 'SALES']    
+
+X = data[['TV', 'RADIO', 'NEWSPAPER']].values
+y = data['SALES'].values
+  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1) 
+
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+
+df.plot(kind='bar',figsize=(10,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+print(f'Accuracy : {round((model.score(X_test, y_test))*100,3)} %')
+predictedSales = regressor.predict([[ 50 , 0,  0]])
+success = predictedSales[0]
+successRate = str(round(success, 2))+  ' %' 
+successRate
