b4190dcda7646302c0e521897716ab3a16fb4c48
Sajitha Suranjana De SIlva <47180074+sajithads@users.noreply.github.com>
2020-04-25 23:52:45 +0530

    JWT auth middleware

--- restful/api/middleware/check-auth.js
+++ restful/api/middleware/check-auth.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "secret");
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "No token provided" });
+    }
 
 };
