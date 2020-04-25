87a0f729b1664895217bd7c4761b68693c3cf80e
Sajitha Suranjana De SIlva <47180074+sajithads@users.noreply.github.com>
2020-04-25 23:59:31 +0530

    user and vendor retrieval routes updated with jwt

--- restful/api/routes/users.js
+++ restful/api/routes/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require('../controllers/users');
+const authorize = require("../middleware/check-auth");
 
-router.get('/match/:email', UserController.get_matching_user);
+
+router.get('/match/:email', authorize, UserController.get_matching_user);
 
 module.exports = router;
