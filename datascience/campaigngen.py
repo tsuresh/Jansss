a3284b163cafd5e3160242a83723ce57cfe68adc
Suresh Peiris <tsmpmail@gmail.com>
2020-04-26 00:23:39 +0530

    Added the base campaign generator file

---
+++ datascience/campaigngen.py
@@ -0,0 +1,18 @@
+from Outcomes import Outcomes
+from audiencePredict import Audience
+
+# Predict audiences [audience dropdown, ]
+
+# Get user interests [desc]
+audiences = Audience()
+
+# Get spending abilities
+
+# Campaign durations [occupations, ]
+
+# Predict competitors [category, price] - DONE
+
+# Get marketing methods and outcome
+outcomes = Outcomes()
+
+print(outcomes.predict(1000, 20, 30))
