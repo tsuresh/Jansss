108bad53d11081321a6644b1528a0db4aae92e61
Sajitha Suranjana De SIlva <47180074+sajithads@users.noreply.github.com>
2020-04-30 20:56:12 +0530

    user update controller module added

--- restful/api/controllers/users.js
+++ restful/api/controllers/users.js
@@ -8,3 +8,39 @@ exports.get_matching_user = (req, res, next) => {
             res.status(400).send("Something went wrong." + err);
         })
 };
+
+exports.update_matching_user = (req, res, next) => {
+    User.find( { _id: req.params.id } )
+      .exec()
+      .then(user => {
+        if (user.length >= 1) {
+          const user = new User({
+            email: req.body.email,
+            // password: hash,
+            firstName: req.body.firstName,
+            surName: req.body.surName,
+            subscription: req.body.subscription,
+            paymentMethod: req.body.paymentMethod
+          });
+         // console.log(req.params.id)
+          User.updateOne({ _id: req.params.id }, user).then(
+            () => {
+              res.status(201).json({
+                message: 'User updated successfully!'
+              });
+            }
+          ).catch(
+            (error) => {
+              res.status(400).json({
+                error: error
+              });
+            }
+          );
+        } else {
+          return res.status(409).json({
+            message: "User not found"
+          });
+        }
+      });
+  }
+  
