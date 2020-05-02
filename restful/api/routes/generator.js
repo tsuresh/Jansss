9a8a4acf90a62488c405046bf4a19fea88e03c28
Suresh Peiris <tsmpmail@gmail.com>
2020-05-02 14:04:21 +0530

    Added campaign generator endpoints

--- restful/api/routes/generator.js
+++ restful/api/routes/generator.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const GeneratorController = require('../controllers/generator');
+const authorize = require("../middleware/check-auth");
 
-router.post('/analytics', GeneratorController.get_analytics);
+router.post('/competitors', authorize, GeneratorController.get_competitors);
 
-router.post('/competitors', GeneratorController.get_competitors);
-
-router.post('/plan', GeneratorController.get_plan);
+router.post('/plan', authorize, GeneratorController.get_plan);
 
 module.exports = router;
